64850a0a28a9d8b79c472f9f2b9994a09f61502b
rongwq <91956>
2026-03-11 18:44:18 +0800

    注册送积分

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/data/UserData.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/data/UserData.java
@@ -53,6 +53,26 @@ public interface UserData {
     }
 
     @Data
+    class ActivityRegUserParam {
+        @Xss(message = "用户账号不能包含脚本字符")
+        @NotBlank(message = "用户账号不能为空")
+        @Size(min = 0, max = 30, message = "用户账号长度不能超过30个字符")
+        @ApiModelProperty(value = "用户名")
+        private String userName;
+
+        @NotBlank(message = "密码不能为空")
+        @ApiModelProperty(value = "密码")
+        private String password;
+
+        @NotBlank(message = "手机号码不能为空")
+        @ApiModelProperty(value = "手机号码")
+        private String phonenumber;
+
+        @ApiModelProperty(value = "用户邮箱")
+        private String email;
+    }
+
+    @Data
     class UserQuery {
         @ApiModelProperty(value = "用户")
         private SysUser user;
